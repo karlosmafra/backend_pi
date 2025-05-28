e6a4eaa6b8280829eef8e84206a8c10b170f0de5
karlosmafra <kadumaframoraes@gmail.com>
2025-05-28 15:12:25 -0300

    Configuracao do banco de dados

--- src/config/db.js
+++ src/config/db.js
@@ -1,1 +1,92 @@
 // Configuração do banco de dados
+const { Pool } = require("pg");
+
+async function connect() {
+  if (global.connection) return global.connection;
+
+  const pool = new Pool({
+    connectionString: process.env.CONNECTION_STRING,
+  });
+
+  const client = await pool.connect();
+  console.log("Banco conectado:", (await client.query("SELECT now()")).rows[0]);
+  client.release();
+
+  global.connection = pool;
+  return pool;
+}
+
+// Funções de SELECT para cada tabela
+async function selectAvaliacoes() {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM avaliacao");
+  return res.rows;
+}
+
+async function selectCategorias() {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM categoria");
+  return res.rows;
+}
+
+async function selectClientes() {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM cliente");
+  return res.rows;
+}
+
+async function selectLogAcoes() {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM log_acao");
+  return res.rows;
+}
+
+async function selectMensagens() {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM mensagem");
+  return res.rows;
+}
+
+async function selectPrestadores() {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM prestador");
+  return res.rows;
+}
+
+async function selectRoles() {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM roles");
+  return res.rows;
+}
+
+async function selectServicos() {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM servico");
+  return res.rows;
+}
+
+async function selectSolicitacoesServico() {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM solicitacao_servico");
+  return res.rows;
+}
+
+async function selectUsuarios() {
+  const client = await connect();
+  const res = await client.query("SELECT * FROM usuario");
+  return res.rows;
+}
+
+module.exports = {
+  connect,
+  selectAvaliacoes,
+  selectCategorias,
+  selectClientes,
+  selectLogAcoes,
+  selectMensagens,
+  selectPrestadores,
+  selectRoles,
+  selectServicos,
+  selectSolicitacoesServico,
+  selectUsuarios,
+};
--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 // Importar e instanciar express
 const express = require('express')
+const db = require("./config/db")
 const app = express()
-const cors = require('cors');
+const cors = require('cors')
 
 // Definir porta
 require('dotenv').config()
 const PORT = process.env.PORT
 
-//const cors = require('cors');
-
+// Rotas
 const UsuarioRotas = require('./routes/usuario')
 
 app.use(cors())
 app.use(express.json())
 app.use('/', UsuarioRotas)
 
-// Iniciar servidor
-app.listen(PORT, () => {
-    console.log(`Servidor aguardando requisições na porta ${PORT}`)
-})
+// Conectar com banco de dados e iniciar servidor
+db.connect().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
+});
